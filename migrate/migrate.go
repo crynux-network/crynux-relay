bfac8475fdc7df4d0418479acb93109fe61af538
lukew <lukew@crynux.io>
2026-08-12 10:47:20 +0800

    [feature] data migration for new contracts

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -126,4 +126,5 @@ func InitMigration(db *gorm.DB) {
 	migrationScripts = append(migrationScripts, migrations.M20260806(db))
 	migrationScripts = append(migrationScripts, migrations.M20260808(db))
 	migrationScripts = append(migrationScripts, migrations.M20260809(db))
+	migrationScripts = append(migrationScripts, migrations.M20260812(db))
 }
